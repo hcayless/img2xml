0a27f133201154b4e7d64257a476e4bbf9aa2d4a
Hugh Cayless <philomousos@gmail.com>
2009-03-12 13:28:38 -0400

    Added license.

--- bin/analysesvg.py
+++ bin/analysesvg.py
@@ -4,7 +4,21 @@
 getrectangles.py
 
 Created by Hugh Cayless on 2008-07-25.
-Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+Copyright (c) 2008 Hugh A. Cayless
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+.
 """
 
 import sys
